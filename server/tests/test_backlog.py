ca54adf022548c5dbc4766a47b4747b9cf5294e4
aithassouelias <aithassouelias@gmail.com>
2024-11-23 13:55:48 +0100

    [BACKLOG] ajout fonction test et GitHub Actions

--- server/tests/test_backlog.py
+++ server/tests/test_backlog.py
@@ -1,3 +1,28 @@
 from functions.backlog import backlog_json_to_df
 import pytest
 
+def test_backlog_valid():
+    """
+    Cette fonction teste
+    """
+    # Test avec un fichier valide
+    valid_file = "../files/backlog.json"
+    df = backlog_json_to_df(valid_file)
+    assert not df.empty  # Vérifie que le DataFrame n'est pas vide
+    assert list(df.columns) == ["en_tant_que", "fonctionnalite", "objectif"]  # Colonnes attendues
+
+def test_backlog_invalid_structure():
+    """
+    """
+    # Test avec un fichier JSON invalide
+    invalid_file = "../files/backlog_invalid.json"
+    df = backlog_json_to_df(invalid_file)
+    assert df.empty  # Doit retourner un DataFrame vide
+
+def test_backlog_invalid_extension():
+    """
+    """
+    # Test avec un fichier avec une mauvaise extension
+    invalid_extension_file = "../files/backlog.txt"
+    df = backlog_json_to_df(invalid_extension_file)
+    assert df.empty  # Doit retourner un DataFrame vide
