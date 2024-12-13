f291f70f2c786fc145adfb35c9307a8668dcaec9
Afdal-B <159702730+Afdal-B@users.noreply.github.com>
2024-12-13 15:45:15 +0100

    add routing

--- client/src/App.js
+++ client/src/App.js
@@ -2,17 +2,18 @@ import LandingPage from "./pages/LandingPage";
 import CreateRoomPage from "./pages/CreateRoomPage";
 import JoinRoomPage from "./pages/JoinRoomPage";
 import UserInfosPage from "./pages/UserInfosPage";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, BrowserRouter } from "react-router";
 export default function App() {
   return (
     <div>
-      <CreateRoomPage></CreateRoomPage>
-      {/*<Routes>
-      <Route path="/" element={<LandingPage />} />
-      <Route path="/create-room" element={<CreateRoomPage />} />
-      <Route path="/join-room" element={<JoinRoomPage />} />
-      <Route path="/profile" element={<UserInfosPage />} />
-    </Routes>*/}
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/create-room" element={<CreateRoomPage />} />
+          <Route path="/join-room" element={<JoinRoomPage />} />
+          <Route path="/profile" element={<UserInfosPage />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
